cf39bb706e578fe704efaf36b7f36861db51e62f
Daniyar <doni2225@mail.ru>
2024-07-10 23:42:52 +0600

    Task01

---
+++ Семинар ЗЯП4 Функции/Task1/Program.cs
@@ -0,0 +1,17 @@
+﻿
+void Main()
+{
+    int value = ReadInt();
+    Console.WriteLine(value);
+}
+
+// тип возвращаемого значения _имя функции(){}
+
+int ReadInt()
+{
+    Console.Write("Введите число: ");
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+Main();

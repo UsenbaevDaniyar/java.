e0469bd615b4e618d104728a22c6d292fcc1c003
Daniyar <doni2225@mail.ru>
2024-07-13 00:57:29 +0600

    Task5 Lesson4

---
+++ Семинар ЗЯП4 Функции/Task5/Program.cs
@@ -0,0 +1,57 @@
+﻿
+void Main()
+{
+    int arraySise = ReadInt("Введите размер массива: ");
+
+    if(arraySise > 8 || arraySise < 1)
+    {
+        Console.Write("Введено некоректный размер массива");
+        return;
+    }
+    int[] array = GenerateArray(arraySise, 0, 9);
+    PrintArray(array);
+
+    int result = Convert.ToInt32(string.Concat(array));
+    
+    // int result = Convert.ToInt32(string.Join("", array));
+    Console.WriteLine(result);
+    // Console.WriteLine(fromArrayToNumber(array));
+}
+
+// int fromArrayToNumber(int[] array)
+// { int number = 0;
+//     for(int i = 0; i < array.Length; i++)
+//     {
+//         number *= 10;
+//         number += array[i];
+//     }
+//     return number;
+// }
+
+
+void PrintArray(int[] arrayForPrint)
+{
+    Console.WriteLine(string.Join(" ", arrayForPrint));
+}
+
+
+int[] GenerateArray(int Sise, int minvalue, int maxvalue)
+
+{
+    int[] TempArray = new int[Sise]; 
+    Random rand = new Random();
+
+    for(int i = 0; i < TempArray.Length; i++)
+    TempArray[i] = rand.Next(minvalue, maxvalue + 1);
+    return TempArray;
+
+}
+
+int ReadInt(string msg)
+{
+    Console.Write(msg);
+   return Convert.ToInt32(Console.ReadLine());
+    
+}
+
+Main();

feff818faa225d1a3602fe2872ea2e2d8da9bb51
Daniyar <doni2225@mail.ru>
2024-07-12 22:40:05 +0600

    Funktion

--- Семинар ЗЯП4 Функции/Task1/Program.cs
+++ Семинар ЗЯП4 Функции/Task1/Program.cs
@@ -1,17 +1,39 @@
 ﻿
 void Main()
 {
-    int value = ReadInt();
-    Console.WriteLine(value);
+    int arraySise = ReadInt("Введите размер массива: ");
+    int[] array = GenerateArray(arraySise, 0, 10);
+    PrintArray(array);
+
+    int[] array2 = GenerateArray(7, 10, 100);
+    PrintArray(array2);
 }
 
-// тип возвращаемого значения _имя функции(){}
-
-int ReadInt()
+void PrintArray(int[] arrayForPrint)
 {
-    Console.Write("Введите число: ");
-    int number = Convert.ToInt32(Console.ReadLine());
-    return number;
+    for(int i  = 0; i < arrayForPrint.Length; i++)
+    Console.Write(arrayForPrint[i] +  " ");
+
+    Console.WriteLine();
+}
+
+int[] GenerateArray(int Sise, int minvalue, int maxvalue)
+
+{
+    int[] TempArray = new int[Sise]; 
+    Random rand = new Random();
+
+    for(int i = 0; i < TempArray.Length; i++)
+    TempArray[i] = rand.Next(minvalue, maxvalue);
+    return TempArray;
+
+}
+
+int ReadInt(string msg)
+{
+    Console.Write(msg);
+   return Convert.ToInt32(Console.ReadLine());
+    
 }
 
 Main();
--- Семинар ЗЯП4 Функции/Task1/obj/Debug/net8.0/Task1.AssemblyInfo.cs
+++ Семинар ЗЯП4 Функции/Task1/obj/Debug/net8.0/Task1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f49bb4a89a02e5310de502f4a54e33386b1a5c3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf39bb706e578fe704efaf36b7f36861db51e62f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

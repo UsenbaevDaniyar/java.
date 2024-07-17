4d98df420fba8e12e88973b5b34434ab01955e2c
Daniyar <doni2225@mail.ru>
2024-07-17 21:39:35 +0600

    lesson 4 Task1

---
+++ Лекция ЗЯП3 Двумерные массивы/Task2/Program.cs
@@ -0,0 +1,30 @@
+﻿int[,] GreateMatrix(int rowCount, int columsCount)
+{
+    int[,] matrix = new int[rowCount, columsCount];
+
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(1, 11);
+        }
+    }
+    return matrix;
+}
+
+void shouMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] matrix = GreateMatrix (4, 5);
+shouMatrix(matrix);

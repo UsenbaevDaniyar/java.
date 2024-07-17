96194dc573df8e1bd6ab20ddd95b0a5127ee49fb
Daniyar <doni2225@mail.ru>
2024-07-18 00:51:33 +0600

    Lesson4 Task4

---
+++ Лекция ЗЯП3 Двумерные массивы/Task4/Program.cs
@@ -0,0 +1,17 @@
+﻿string getLetters(string s)
+{
+    string letters = "";
+    foreach(char e in s)
+    {
+        if(char.IsAsciiLetter(e) == true)
+        {
+            letters = letters + e;
+        }
+    }
+
+    return letters;
+}
+
+string str = Console.ReadLine();
+string result = getLetters(str);
+Console.WriteLine(result);

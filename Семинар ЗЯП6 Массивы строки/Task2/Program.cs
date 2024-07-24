0ff75101865ec8df27f0cc22a040c61d1c8e00dd
Daniyar <doni2225@mail.ru>
2024-07-24 23:02:05 +0600

    Task2

---
+++ Семинар ЗЯП6 Массивы строки/Task2/Program.cs
@@ -0,0 +1,26 @@
+﻿void Main()
+{
+    string str = "text";
+
+    char[] chars = StringToCharArray(str);
+
+    PrintCharArray(chars);
+
+}
+
+char[] StringToCharArray(string str)
+{
+    char[] chars =  new char[str.Length];
+
+    for(int i = 0; i < str.Length; i++)
+        chars[i] = str[i];
+
+        return chars;
+}
+
+void PrintCharArray(char[] chars)
+{
+    Console.WriteLine(string.Join(", "), chars);
+}
+
+Main();

18c0c80c2e41ac1de89c70af827954e2b3e3331d
Daniyar <doni2225@mail.ru>
2024-07-24 22:29:03 +0600

    Task1

---
+++ Семинар ЗЯП6 Массивы строки/Task1/Program.cs
@@ -0,0 +1,26 @@
+﻿void Main()
+{
+    char[] chars = {'a', 'b', 'c', 'd'};
+    // 1-ый способ
+    // string str = new string(chars);
+    // Console.WriteLine(str);
+
+    // Console.WriteLine(CharsToString(chars));
+    // 3-й способ
+    // Console.WriteLine(string.Join("", chars));
+    // 4-й способ
+    Console.WriteLine(string.Concat(chars));
+}
+// 2-й способ
+// string CharsToString(char[] chars)
+
+// {
+//     string str = "12345";
+
+//     foreach (char c in chars)
+//         str += c;
+
+//     return str;
+// }
+
+Main();

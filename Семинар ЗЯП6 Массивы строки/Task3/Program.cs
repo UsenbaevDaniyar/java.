3fa99f7815ac82934f395a4989e5003187653e27
Daniyar <doni2225@mail.ru>
2024-07-25 22:51:05 +0600

    Task2

---
+++ Семинар ЗЯП6 Массивы строки/Task3/Program.cs
@@ -0,0 +1,27 @@
+﻿void Main()
+{
+    string str = "textabc";
+
+    char[] chars = StringToCharArray(str);
+
+    PrintCharArray(chars);
+
+}
+
+char[] StringToCharArray(string str)
+
+{
+    char[] chars = new char[str.Length];
+
+    for(int i = 0; i < str.Length; i++)
+    chars[i] = str[i];
+
+    return chars;
+}
+
+void PrintCharArray(char[] chars)
+{
+    Console.WriteLine(string.Join(", ", chars));
+}
+
+Main();
